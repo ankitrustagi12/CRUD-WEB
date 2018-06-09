4d895d847c703ab1b96788762ed241e5c2504235
Ankit rustagi <ankitrustagi12@gmail.com>
2018-06-10 01:01:11 +0530

    crudapp

--- crud/config/db.php
+++ crud/config/db.php
@@ -1,26 +1,22 @@
 <?php
-
 ob_start();
 
-if(!isset($_session))
-{
+
+if(!isset($_SESSION)){
     session_start();
 }
 
+
 $host = 'localhost';
 $user = 'root';
-$pass ='';
-$db_name ='crudapp';
+$pass = '';
+$db_name = 'crudapp';
 
-$connection=mysqli_connect($host,$user,$pass,$db_name);
+$connection = mysqli_connect($host, $user, $pass, $db_name);
 
-if($connection)
-{
-    echo "Connection Successful";
+if(!$connection){
+    die("CONNECTION TO DB FAILED. " . mysqli_error($connection));
 }
-else
-{
-    echo "Connection Failed";
-}
+
 
 ?>
